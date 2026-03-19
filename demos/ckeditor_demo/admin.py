97ea79f9608732772b741ae10422e1c8ca509ca5
Nina Belyavskaya <ninaobel@gmail.com>
2026-03-19 18:21:37 +0100

    Demo with Ckeditor 5

--- demos/ckeditor_demo/admin.py
+++
@@ -1,8 +0,0 @@
-# demo versuion of admin.py for ckeditor example
-from django.contrib import admin
-from models import Document
-
-
-@admin.register(Document)
-class DocumentAdmin(admin.ModelAdmin):
-    list_display = ("id", "title")
